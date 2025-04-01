b5afb9ce6245eda5865d6d500ecaad0b559762b1
Javsan0424 <A01198909@tec.mx>
2025-03-31 19:01:37 -0600

    The controllers prints only the pair tweets

--- src/controllers/tweet.ts
+++ src/controllers/tweet.ts
@@ -1,6 +1,22 @@
+import Tweetdatabase from "../db/tweet";
+
+const tweetdatabase = new Tweetdatabase()
+
 class TweetController {
+
     async getAllTweets() {
-        return [1,2,3,4,5,6,7,8,9,10]
+        const tweet = await tweetdatabase.tweet_db();
+
+        let returned_tweet = []
+
+        for(let i = 0; i < tweet.length; i++){
+            if(tweet[i] % 2 === 0){
+                returned_tweet.push(tweet[i]);
+                
+            }
+        }
+
+        return returned_tweet;
     }
 }
 
